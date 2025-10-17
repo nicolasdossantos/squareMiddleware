b5f3cf0a32fc5f1427c984e7f7f703bd31c25c23
Nicolas dos Santos <nickdossantos@mac.lan>
2025-10-17 14:30:32 -0400

    Cleanup: Remove node_modules/ and deploy/ from Git tracking
    - Added deploy/ to .gitignore
    - Removed 79,248 node_modules files from Git (kept on disk)
    - Removed 5 deploy/ files from Git (kept on disk)
    - These directories should never have been committed to version control

--- node_modules/testcontainers/build/generic-container/inspect-container-util-ports-exposed.d.ts
+++
@@ -1,9 +0,0 @@
-import { ContainerInspectInfo } from "dockerode";
-import { InspectResult } from "../types";
-import { PortWithOptionalBinding } from "../utils/port";
-type Result = {
-    inspectResult: ContainerInspectInfo;
-    mappedInspectResult: InspectResult;
-};
-export declare function inspectContainerUntilPortsExposed(inspectFn: () => Promise<ContainerInspectInfo>, ports: PortWithOptionalBinding[], containerId: string, timeout?: number): Promise<Result>;
-export {};
